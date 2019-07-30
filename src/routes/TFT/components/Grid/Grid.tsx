78c62a9a3498b7a659bf881f6a9dc233761f7ee2
Renato Iwashima <renatoiwa@gmail.com>
2019-07-29 19:02:42 -0700

    Builds WIP

---
+++ src/routes/TFT/components/Grid/Grid.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import cx from "classnames";
+import styles from "./Grid.module.css";
+
+const xOffset = 11.5;
+const yOffset = 20;
+
+interface PodProps {
+  readonly x: number;
+  readonly y: number;
+}
+const Pod: React.FC<PodProps> = ({ x, y }) => {
+  return (
+    <use
+      className={cx(styles.hex, { [styles.hexEnemy]: y < 3 })}
+      xlinkHref="#pod"
+      transform={`translate(${xOffset * x * 2 + (!(y % 2) ? xOffset : 0)}, ${y *
+        yOffset})`}
+    />
+  );
+};
+
+export const Grid: React.FC = () => {
+  const pods = [];
+  for (let y = 0; y < 6; y++) {
+    for (let x = 0; x < 7; x++) {
+      pods.push(<Pod key={`${x},${y}`} x={x} y={y} />);
+    }
+  }
+  return (
+    <div className={styles.gridContainer}>
+      <div className={styles.gridChampion}>
+        <span
+          className={styles.gridChampionAvatar}
+          style={{
+            backgroundImage: `url(${
+              process.env.PUBLIC_URL
+            }/tft/tft_mordekaiser.png)`
+          }}
+        />
+        <span className="VisuallyHidden">Mordekaiser</span>
+      </div>
+      <svg viewBox="0 0 140 110" className={styles.svg}>
+        <defs>
+          <g id="pod">
+            <polygon points="11 1.5 21.8 7.8 21.8 20.3 11 26.5 0.2 20.3 0.2 7.8" />
+          </g>
+        </defs>
+        <g className={styles.hexGroup}>{pods}</g>
+      </svg>
+    </div>
+  );
+};
--- src/routes/TFT/containers/Builds.tsx
+++ src/routes/TFT/containers/Builds.tsx
@@ -3,6 +3,7 @@ import { PageContainer } from "../../../components/PageContainer/PageContainer";
 import { Helmet } from "react-helmet";
 import { RouteComponentProps } from "react-router";
 import ReactGA from "react-ga";
+import { Grid } from "../components/Grid/Grid";
 
 export interface TFTBuildsProps extends RouteComponentProps {}
 const TFTBuilds: React.FC<TFTBuildsProps> = ({ match }) => {
@@ -16,11 +17,71 @@ const TFTBuilds: React.FC<TFTBuildsProps> = ({ match }) => {
           content="Detailed step-by-step builds / team compositions guides for Teamfight Tactics (TFT)"
         />
       </Helmet>
-      <h1>Builds</h1>
-      <p>
-        Builds coming soon... This section will include detailed build guides
-        for the current patch.
-      </p>
+      <h1>Phantom Glacial Rangers</h1>
+      <ol>
+        <li>
+          <a href="#team-composition">Team composition</a>
+        </li>
+        <li>
+          <a href="#team-composition">Items</a>
+        </li>
+        <li>
+          <a href="#team-composition">Positioning</a>
+        </li>
+        <li>
+          <a href="#team-composition">Pros</a>
+        </li>
+        <li>
+          <a href="#team-composition">Cons</a>
+        </li>
+        <li>
+          <a href="#team-composition">Build guide</a>
+        </li>
+      </ol>
+      <h2 id="team-composition">1. Team composition</h2>
+      <ul>
+        <li>Mordekaiser</li>
+        <li>Vayne</li>
+        <li>Varus</li>
+        <li>Ashe</li>
+        <li>Volibear</li>
+        <li>Kindred</li>
+        <li>Sejuani</li>
+        <li>Anivia</li>
+      </ul>
+      <h2>2. Items</h2>
+      <p>Detailed build item map here</p>
+      <h2>3. Positioning</h2>
+      <Grid />
+      <h2>4. Pros</h2>
+      <ul>
+        <li>Forte controle de campo;</li>
+        <li>Sobrevivência extra graças à habilidade de Kindred;</li>
+        <li>Neutralização de um campeão graças ao bônus de phantom;</li>
+        <li>Baixa necessidade de mudança no posicionamento;</li>
+        <li>Forte contra inimigos agrupados;</li>
+      </ul>
+      <h2>5. Cons</h2>
+      <ul>
+        <li>
+          Build altamente dependente do Kindred (para os bônus de ranger e
+          phantom);
+        </li>
+        <li>
+          Build difícil de se obter eficiência completa (necessita nível 8 ou
+          Force of Nature);
+        </li>
+        <li>4th glacial e ranger obtíveis apenas a partir do nível 5;</li>
+        <li>
+          4th bônus glacial dependente de nível do jogador ou Force of Nature;
+        </li>
+        <li>Campeões de custo médio para alto (custo mediano: $3);</li>
+        <li>
+          Eficiência de controle e de dano reduzidos contra campeãos espalhados.
+        </li>
+      </ul>
+      <h2>6. Build guide</h2>
+      <p>Detailed guide here</p>
     </PageContainer>
   );
 };
